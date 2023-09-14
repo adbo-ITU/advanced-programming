56f5a39822580379f372d8c5993c0354823e9c78
Adrian Borup <adrian@borup.com>
2023-09-14 13:22:26 +0200

    Add exercise 4

--- 030-option/Exercises.scala
+++ 030-option/Exercises.scala
@@ -56,7 +56,10 @@ object Tree:
 
   // Exercise 4
 
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    t match
+      case Leaf(v)      => Leaf(f(v))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
 
   // Exercise 5
 
