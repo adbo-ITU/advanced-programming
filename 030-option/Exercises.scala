a7e9bd681f977a6403c1e971c1fb13d726509680
Adrian Borup <adrian@borup.com>
2023-09-14 14:48:25 +0200

    Add exercise 9

--- 030-option/Exercises.scala
+++ 030-option/Exercises.scala
@@ -119,7 +119,7 @@ object Option:
   // Exercise 9
 
   def map2[A, B, C](ao: Option[A], bo: Option[B])(f: (A, B) => C): Option[C] =
-    ???
+    ao.flatMap(a => bo.map(b => f(a, b)))
 
   // Exercise 10
 
