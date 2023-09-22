c774238d4ef6a47b6edac7c66a6dad72a7cee5d3
Florian Biermann <flbm@simcorp.com>
2023-09-22 12:42:43 +0200

    Use covariance and demonstrate which expressions typecheck

--- 040-lazy-lists/EmptyType.scala
+++ 040-lazy-lists/EmptyType.scala
@@ -11,8 +11,8 @@ enum Op:
   case Div
 
 // An ADT modeling an abstract syntax tree. Note the seemingly useless
-// contra-variant type parameter A
-enum AST[-A]:
+// co-variant type parameter A
+enum AST[+A]:
   case ConstInt(c : Int) extends AST[Nothing]
   case BinOp[A](a : A, op : Op, lhs : AST[A], rhs : AST[A]) extends AST[A]
 
@@ -27,17 +27,28 @@ enum AST[-A]:
 // - BinOp needs an instance of A - in this case the only value inhabiting Unit, ().
 type Expr = AST[Unit]
 
+// Short-hand for constructing binary expressions.
+def binop(op : Op, lhs : Expr, rhs : Expr) : Expr =
+  AST.BinOp((), op, lhs, rhs)
+
 // This type is not inhabited by BinOp, because that would require to
 // call the BinOp constructor with a value of type EmptyType, which we
 // cannot construct.
 // Note: no sub-typing relation between ValueA and Expr
 type ValueA = AST[EmptyType]
 
+// Short-hand to construct a constant expression. Try to change the
+// return type from Expr to ValueA and recompile.
+def constA(x : Int) : Expr = AST.ConstInt(x)
+
 // This type is not inhabited by BinOp, because that would require to
 // call the BinOp constructor with a value of type Nothing, which we
 // cannot construct.
-// Note: Expr <: ValueB because Nothing <: Unit
+// Note: ValueB <: Expr because Nothing <: Unit
 type ValueB = AST[Nothing]
+
+// Short-hand to construct a constant expression.
+def constB(x : Int) : ValueB = AST.ConstInt(x)
 
 // The following two functions are now guaranteed to never fail, but
 // unfortunately, the Scala compiler does not see that.
@@ -54,7 +65,16 @@ def toIntB(c : ValueB) : Int = c.toInt()
 
 // If you are curious, try to implement an evaluation function for
 // each variant. There is a subtle difference between evalA and evalB
-// owned to contra-variance.
+// owned to co-variance.
 def evalA(e : Expr) : ValueA = ???
 
 def evalB(e : Expr) : ValueB = ???
+
+val e1 = binop(Op.Add, constA(1), constA(2))
+val e2 = binop(Op.Add, constB(1), constB(2))
+// val _ = toIntA(constA(1))     // Does not typecheck.
+val _ = toIntA(evalA(constA(1))) // Typechecks.
+val _ = toIntB(constB(1))        // Typechecks.
+val _ = toIntB(evalB(constB(1))) // Typechecks.
+// val _ = toIntA(e1)            // Does not typecheck.
+// val _ = toIntB(e2)            // Does not typecheck.
