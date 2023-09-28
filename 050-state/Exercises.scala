bb145943d554ee0eaf264c86bee630add335c1b2
Adrian Borup <adrian@borup.com>
2023-09-28 15:23:03 +0200

    Add exercise 6

--- 050-state/Exercises.scala
+++ 050-state/Exercises.scala
@@ -79,7 +79,11 @@ object RNG:
   // Exercise 6
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    ???
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a, b), rng2)
+    }
 
   // Exercise 7
 
