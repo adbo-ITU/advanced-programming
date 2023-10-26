62453fd7949226310c50e21309a16512ccf46ef5
Adrian Borup <adrian@borup.com>
2023-10-26 13:43:58 +0200

    Autoformat

--- 080-prop/Exercises.scala
+++ 080-prop/Exercises.scala
@@ -20,24 +20,25 @@ lazy val y = rng2.nextInt._1
 // Exercise 3
 
 object Exercise_3:
-  
+
   import org.scalacheck.Prop.{forAll, forAllNoShrink}
- 
+
   // Use this generator explicitly in the two properties
-  val intList = 
-    org.scalacheck.Gen.listOf(org.scalacheck.Gen.choose(0, 100))
-      .suchThat (_.nonEmpty)
+  val intList =
+    org.scalacheck.Gen
+      .listOf(org.scalacheck.Gen.choose(0, 100))
+      .suchThat(_.nonEmpty)
 
   // The properties are put into a function taking `minimum` as argument to
   // allow the teachers to test them with different mutants of `minimum`.
-  
-  def p1Min (minimum: List[Int] => Int): org.scalacheck.Prop = 
+
+  def p1Min(minimum: List[Int] => Int): org.scalacheck.Prop =
     org.scalacheck.Prop.forAll(intList) { (l: List[Int]) =>
       val min = minimum(l)
       l.forall(_ >= min)
     }
 
-  def p2Min (minimum: List[Int] => Int): org.scalacheck.Prop = 
+  def p2Min(minimum: List[Int] => Int): org.scalacheck.Prop =
     org.scalacheck.Prop.forAll(intList) { (l: List[Int]) =>
       val min = minimum(l)
       l.exists(_ == min)
@@ -54,7 +55,7 @@ object Exercise_4:
     self => // TODO remove
     def check: Boolean
 
-    infix def && (that: Prop): Prop =
+    infix def &&(that: Prop): Prop =
       new Prop:
         def check = self.check && that.check
 
@@ -63,39 +64,40 @@ end Exercise_4
 opaque type Gen[+A] = State[RNG, A]
 
 object Gen:
-  
-  extension [A](g: Gen[A]) 
+
+  extension [A](g: Gen[A])
     // Let's convert generator to streams of generators
-    def toLazyList (rng: RNG): LazyList[A] =
-      LazyList.unfold[A,RNG](rng)(rng => Some(g.run(rng)))
+    def toLazyList(rng: RNG): LazyList[A] =
+      LazyList.unfold[A, RNG](rng)(rng => Some(g.run(rng)))
 
   // Exercise 5
 
-  def choose (start: Int, stopExclusive: Int): Gen[Int] =
-    State(nonNegativeInt).map(i => (stopExclusive - start) % stopExclusive + start)
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    State(nonNegativeInt).map(i =>
+      (stopExclusive - start) % stopExclusive + start
+    )
 
   // Exercise 6
 
-  def unit[A] (a: =>A): Gen[A] = 
+  def unit[A](a: => A): Gen[A] =
     ???
 
-  def boolean: Gen[Boolean] = 
+  def boolean: Gen[Boolean] =
     ???
 
-  def double: Gen[Double] = 
+  def double: Gen[Double] =
     ???
 
   // Exercise 7
-  
-  extension [A](self: Gen[A])
 
+  extension [A](self: Gen[A])
     def listOfN(n: Int): Gen[List[A]] =
       ???
 
   // Exercise 8
- 
+
   // Write here ... ???
-  
+
   // Exercise 9
 
   extension [A](self: Gen[A])
@@ -104,9 +106,8 @@ object Gen:
       ??? // todo: rerun ex 05 tests
 
     // It will be convenient to also have map (uses flatMap)
-    def map[B](f: A => B): Gen[B] = 
+    def map[B](f: A => B): Gen[B] =
       self.flatMap { a => unit(f(a)) }
-
 
   // Exercise 10
 
@@ -114,11 +115,10 @@ object Gen:
     def listOf(size: Gen[Int]): Gen[List[A]] =
       ???
 
-
   // Exercise 11
 
   extension [A](self: Gen[A])
-    def union (that: Gen[A]): Gen[A] =
+    def union(that: Gen[A]): Gen[A] =
       ???
 
 end Gen
@@ -128,30 +128,29 @@ import Gen.*
 object Exercise_12:
 
   // Exercise 12 (type classes, givens, using, summon)
- 
+
   // Choose one of the following templates, but note only listOfN and
   // listOf are tested (so rename if you use another than the first)
 
   def listOfN[A: Gen](n: Int): Gen[List[A]] =
     ???
-  
+
   def listOfN_[A: Gen]: Int => Gen[List[A]] =
     ???
-  
-  def listOfN__ [A](n: Int)(using genA: Gen[A]): Gen[List[A]] =
+
+  def listOfN__[A](n: Int)(using genA: Gen[A]): Gen[List[A]] =
     ???
 
   // Choose one of the following templates, but note only listOfN and
   // listOf are tested (so rename if you use another than the first)
-  
+
   def listOf[A: Gen](using genInt: Gen[Int]): Gen[List[A]] =
     ???
-  
-  def listOf_[A] (using genInt: Gen[Int], genA: Gen[A]): Gen[List[A]] =
+
+  def listOf_[A](using genInt: Gen[Int], genA: Gen[A]): Gen[List[A]] =
     ???
 
 end Exercise_12
-
 
 // (Exercise 13)
 // Read the code below (and/or Section 8.1.6). You will find the
@@ -168,7 +167,7 @@ enum Result:
   case Falsified(failure: FailedCase, successes: SuccessCount)
 
   def isFalsified: Boolean = this match
-    case Passed => false
+    case Passed          => false
     case Falsified(_, _) => true
 
 end Result
@@ -182,29 +181,27 @@ def randomLazyList[A](g: Gen[A])(rng: RNG): LazyList[A] =
 
 def buildMsg[A](s: A, e: Exception): String =
   s"test case: $s\n" +
-  s"generated an exception: ${e.getMessage}\n" +
-  s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
 
 def forAll[A](as: Gen[A])(f: A => Boolean): Prop =
-  (max, n, rng) => 
+  (max, n, rng) =>
     randomLazyList(as)(rng)
       .zip(LazyList.from(0))
       .take(n)
       .map { (a, i) =>
-             try if f(a) then Passed else Falsified(a.toString, i)
-             catch case e: Exception => Falsified(buildMsg(a, e), i) }
+        try if f(a) then Passed else Falsified(a.toString, i)
+        catch case e: Exception => Falsified(buildMsg(a, e), i)
+      }
       .find(_.isFalsified)
       .getOrElse(Passed)
 
 def forAllNotSized[A] = forAll[A]
-  
-extension (self: Prop)
-  infix def && (that: Prop): Prop = (maxSize, tcs, rng) => 
-    ???
-  
-  infix def || (that: Prop): Prop = (maxSize, tcs, rng) =>
-    ???
 
+extension (self: Prop)
+  infix def &&(that: Prop): Prop = (maxSize, tcs, rng) => ???
+
+  infix def ||(that: Prop): Prop = (maxSize, tcs, rng) => ???
 
 // Exercise 14
 
@@ -217,12 +214,12 @@ extension [A](self: Gen[A])
 
 // Exercise 15
 
-extension [A](self: Gen[A]) 
+extension [A](self: Gen[A])
   def list: SGen[List[A]] =
     ???
 
 // A sized implementation of prop, takes MaxSize to generate
-// test cases of given size.  
+// test cases of given size.
 //
 // The code below also contains the `run` method for Prop - which
 // provides a simple way to execute tests. Needed in the final
@@ -234,49 +231,56 @@ object SGen:
 
     def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
       (max, n, rng) =>
-        val casesPerSize = (n.toInt - 1)/max.toInt + 1
+        val casesPerSize = (n.toInt - 1) / max.toInt + 1
         val props: LazyList[Prop] =
-          LazyList.from(0)
+          LazyList
+            .from(0)
             .take((n.toInt min max.toInt) + 1)
             .map { i => forAllNotSized(g(i))(f) }
         val prop: Prop =
-          props.map[Prop](p => (max, n, rng) => 
-            p(max, casesPerSize, rng)).toList.reduce(_ && _)
+          props
+            .map[Prop](p => (max, n, rng) => p(max, casesPerSize, rng))
+            .toList
+            .reduce(_ && _)
         prop(max, n, rng)
 
   extension (self: Prop)
     def run(
-      maxSize: MaxSize = 100,
-      testCases: TestCases = 100,
-      rng: RNG = RNG.Simple(System.currentTimeMillis)): Boolean =
-
+        maxSize: MaxSize = 100,
+        testCases: TestCases = 100,
+        rng: RNG = RNG.Simple(System.currentTimeMillis)
+    ): Boolean =
       self(maxSize, testCases, rng) match
-      case Result.Falsified(msg, n) =>
-        println(s"\u001b[34m! Falsified after $n passed tests:\n $msg [message from our Prop framework]")
-        false
-      case Result.Passed =>
-        println(s"\u001b[34m+ OK, passed $testCases tests. [message from our Prop framework]")
-        true
+        case Result.Falsified(msg, n) =>
+          println(
+            s"\u001b[34m! Falsified after $n passed tests:\n $msg [message from our Prop framework]"
+          )
+          false
+        case Result.Passed =>
+          println(
+            s"\u001b[34m+ OK, passed $testCases tests. [message from our Prop framework]"
+          )
+          true
 
 end SGen
 
 // Exercise 16
 
 // Use this generator explicitly in the two properties
-val nonEmptyList: SGen[List[Int]] =  
+val nonEmptyList: SGen[List[Int]] =
   (n: Int) => Gen.choose(-100, 100).listOfN(n max 1)
 
 object Exercise_16:
-  
+
   import SGen.*
 
   // The properties are put into a function taking `minimum` as argument to
   // allow the teachers to test them with different mutants of `minimum`.
-  
-  def p1Min(minimum: List[Int] => Int): Prop = 
+
+  def p1Min(minimum: List[Int] => Int): Prop =
     ???
-  
-  def p2Min(minimum: List[Int] => Int): Prop = 
+
+  def p2Min(minimum: List[Int] => Int): Prop =
     ???
 
 end Exercise_16
