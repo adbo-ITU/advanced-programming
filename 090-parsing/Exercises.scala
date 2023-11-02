dfe73f75daa99d90230b46f7a97f849333a8947f
Adrian Borup <adrian@borup.com>
2023-11-02 15:19:19 +0100

    Add exercise 2

--- 090-parsing/Exercises.scala
+++ 090-parsing/Exercises.scala
@@ -209,7 +209,7 @@ trait Parsers[ParseError, Parser[+_]]:
 
   extension [A](p: Parser[A])
     def many: Parser[List[A]] =
-      ???
+      p.map2(p.many)((a, b) => a :: b).or(succeed(List())).map(_.reverse)
 
   // Exercise 3
 
