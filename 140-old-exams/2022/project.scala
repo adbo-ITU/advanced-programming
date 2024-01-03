8e0df5459b58cfc2efbb4ec1e456cda8378b22ba
Adrian Borup <adrian@borup.com>
2024-01-03 20:04:14 +0100

    Exam 2022: Update project

--- 140-old-exams/2022/project.scala
+++ 140-old-exams/2022/project.scala
@@ -1,7 +1,7 @@
 // I used scala-cli version 0.1.18
-//> using scala "3.1.3"
-//> using options "-deprecation", "-feature", "-source:future", "-language:adhocExtensions"
-//> using lib "org.scalacheck::scalacheck:1.17.0"
-//> using lib "org.scalactic::scalactic:3.2.14"
-//> using lib "dev.optics::monocle-core:3.1.0"
-//> using lib "dev.optics::monocle-macro:3.1.0"
+//> using scala 3.3.0
+//> using options -Xfatal-warnings, -deprecation, -feature, -source:future, -language:adhocExtensions
+//> using dep "org.scalacheck::scalacheck:1.17.0"
+//> using lib "org.scalactic::scalactic:3.2.17"
+//> using lib "dev.optics::monocle-core:3.2.0"
+//> using lib "dev.optics::monocle-macro:3.2.0"
