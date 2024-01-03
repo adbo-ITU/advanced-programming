60d08d70f27074b1844545e8711e4c48065d97fb
Adrian Borup <adrian@borup.com>
2024-01-03 10:07:44 +0100

    Exam 2021: Autoformat and update 2021 exam

--- 140-old-exams/2021/project.scala
+++ 140-old-exams/2021/project.scala
@@ -1,6 +1,6 @@
-//> using scala "3.1.3"
-//> using lib "org.scalacheck::scalacheck:1.16.0"
-//> using lib "org.scalactic::scalactic:3.2.14"
-//> using options "-deprecation", "-feature", "-source:future", "-language:adhocExtensions"
-//> using lib "dev.optics::monocle-core:3.1.0"
-//> using lib "dev.optics::monocle-macro:3.1.0"
+//> using scala 3.3.0
+//> using options -Xfatal-warnings, -deprecation, -feature, -source:future, -language:adhocExtensions
+//> using dep "org.scalacheck::scalacheck:1.17.0"
+//> using lib "org.scalactic::scalactic:3.2.17"
+//> using lib "dev.optics::monocle-core:3.2.0"
+//> using lib "dev.optics::monocle-macro:3.2.0"
